61d9c45fea2e90cf08f236a7366fa5e4041e212c
Boris Okunskiy <boris@okunskiy.name>
2023-07-24 15:24:49 +0300

    feat: allow CORS_EXPOSE_HEADERS and friends via env

--- src/main/handlers/HttpCorsHandler.ts
+++ src/main/handlers/HttpCorsHandler.ts
@@ -1,3 +1,5 @@
+import { config } from 'mesh-config';
+
 import { HttpContext } from '../HttpContext.js';
 import { HttpHandler, HttpNext } from '../HttpHandler.js';
 
@@ -8,18 +10,27 @@ import { HttpHandler, HttpNext } from '../HttpHandler.js';
  */
 export class HttpCorsHandler implements HttpHandler {
 
+    @config({ default: 'Content-Length,Date' })
+    CORS_EXPOSE_HEADERS!: string;
+
+    @config({ default: 3600 })
+    CORS_MAX_AGE!: number;
+
+    @config({ default: true })
+    CORS_ALLOW_CREDENTIALS!: boolean;
+
     async handle(ctx: HttpContext, next: HttpNext) {
         ctx.setResponseHeader('Vary', 'Origin');
         const origin = ctx.state.corsOrigin ?? ctx.getRequestHeader('Origin');
         if (!origin) {
             return next();
         }
-        const maxAge = ctx.state.corsMaxAge ?? 3600;
-        const exposeHeaders = ctx.state.corsExposeHeaders ?? ['Content-Length', 'Date'];
-        const allowCredentials = ctx.state.corsAllowCredentials ?? true;
+        const maxAge = ctx.state.corsMaxAge ?? this.CORS_MAX_AGE;
+        const exposeHeaders = ctx.state.corsExposeHeaders ?? this.CORS_EXPOSE_HEADERS;
+        const allowCredentials = ctx.state.corsAllowCredentials ?? this.CORS_ALLOW_CREDENTIALS;
         ctx.setResponseHeader('Access-Control-Allow-Origin', origin);
         ctx.setResponseHeader('Access-Control-Max-Age', String(maxAge));
-        ctx.setResponseHeader('Access-Control-Expose-Headers', exposeHeaders.join(','));
+        ctx.setResponseHeader('Access-Control-Expose-Headers', exposeHeaders);
         ctx.setResponseHeader('Access-Control-Allow-Credentials', String(allowCredentials));
         // Respond with 204 to preflight request
         const isPreflight = ctx.method === 'OPTIONS' && !!ctx.getRequestHeader('Access-Control-Request-Method');
